6aac7e9708bdf60afcd4dda25e85c78a44002f9d
Sabrina0410 <sabrina900410@gmail.com>
2016-06-26 15:13:25 +0800

    origin

---
+++ hello_world.rb
@@ -0,0 +1,16 @@
+=begin
+	
+rescue Exception => e
+	
+end
+
+str1="hello"
+str2="world"
+puts "#{str1}#{str2}"
+
+=end
+
+def area_of_circle(radius)
+	return 3.14 * radius * radius
+end
+puts area_of_circle(10)
